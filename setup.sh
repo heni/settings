81f5f188a28f9aaafa6db2997650d6d5b2f4fef8
rutsh <Eugene.Krokhalev@gmail.com>
2017-05-15 14:26:35 +0300

    reusable setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,11 @@ set -eu -o pipefail
 
 pushd $(dirname $0)
 cp -r .vimrc ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+else
+    pushd ~/.vim/bundle/Vundle.vim
+    git pull
+    popd
+fi
 popd
