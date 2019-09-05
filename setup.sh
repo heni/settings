adb1a60a2b965d92b7912ac7b722a6746d2ed9f5
Eugene Krokhalev <Eugene.Krokhalev@gmail.com>
2019-09-05 15:57:15 +0000

    check user rights on install

--- setup.sh
+++ setup.sh
@@ -2,7 +2,11 @@
 set -eu -o pipefail
 
 pushd $(dirname $0)
+
 cp -r .vimrc ~/.vimrc
+touch ~/.viminfo
+chown ${USER} ~/.vimrc ~/.viminfo
+
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 else
